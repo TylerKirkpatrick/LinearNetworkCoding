899b54f165d334e3ac76ffe2bae323cb869ed533
TylerKirkpatrick <tkirkpatrick@smu.edu>
2016-11-04 17:27:03 -0500

    added main.cpp, removed uneccessary files

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,70 @@
-#include "Packet.h"
-#include "Node.h"
+//#include "Node.h"
 #include <string>
 #include <vector>
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
-int main(){
-    //build a network with random number of nodes and random edge weights, ignore the edges with weight more than 10.
-    
-    map path{};
-    Node source;
-    source.distance = 0;
-    path[source.name] = source.distance;
-    while(path is not empty){
-        current = path.pop(path.min());
-        for each node, n adjacent to current:
-            if (n.distance>current.distance+weight of edges from n to current){
-                n.distance = current.distance+weight of edges from n to current;
-                n.parent = current;
-                if (n not in path){
-                    path[n.name] = n.distance;
-                }
-            }
-       
-    }
-    
-    
-    
+//generate network of nodes, connected by edges
+
+void generateGraph(int width, int height, int maxWeight);
+void showGraph(int width, int height);
+
+int globalArray[5][5];
+
+using namespace std;
+
+int main() 
+{
+	generateGraph(5,5,20);
+	showGraph(5,5);
+	//delete globalArray;
+
+}
+
+
+
+
+
+
+
+void generateGraph(int width, int height, int maxWeight) 
+{
+//globalArray = new int[width][height];
+
+for (int i = 0; i < width; i++)
+{
+	for (int j = 0; j < height; j++)
+	{
+		if( i == j)
+		{
+			globalArray[i][j] = 0;
+		}
+		else if(j>i)
+		{
+			int var = rand() % maxWeight + 1;
+			globalArray[i][j] = var;
+			globalArray[j][i] = globalArray[i][j];
+			
+		}
+
+
+		}
+}
+}
+//done generating
+
+void showGraph(int width, int height)
+{
+for (int i = 0; i < height; i++)
+{
+	for (int j = 0; j < width; j++)
+	{
+		
+		cout<<globalArray[i][j]<<", ";
+
+	}
+	cout<<endl<<endl;
+}
+	
 }
